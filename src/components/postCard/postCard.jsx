dd212edad82c1c6cf0cabc982bc0357c8074b41b
VladZhyrnyi <vladzhyrnyi@ukr.net>
2024-01-29 21:13:29 +0200

    add post card component with styles

---
+++ src/components/postCard/postCard.jsx
@@ -0,0 +1,34 @@
+import Image from "next/image";
+import styles from "./postCard.module.css";
+import Link from "next/link";
+
+const PostCard = () => {
+  return (
+    <div className={styles.container}>
+      <div className={styles.top}>
+        <div className={styles.imgContainer}>
+          <Image
+            src="https://images.pexels.com/photos/9316207/pexels-photo-9316207.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"
+            alt=""
+            fill
+            className={styles.img}
+          />
+        </div>
+        <span className={styles.date}>11.11.1111</span>
+      </div>
+      <div className={styles.bottom}>
+        <h1 className={styles.title}>Title</h1>
+        <p className={styles.desc}>
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolor
+          necessitatibus illum suscipit adipisci molestiae, quo quia vel
+          officiis autem dolorum.
+        </p>
+        <Link className={styles.link} href="/blog/post">
+          READ MORE
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default PostCard;
