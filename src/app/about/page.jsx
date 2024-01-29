73c0f1c01fc2b0f4e837ed5682feb3bde9285006
VladZhyrnyi <vladzhyrnyi@ukr.net>
2024-01-29 21:12:39 +0200

    add about page with styles

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -12,7 +12,7 @@ const AboutPage = () => {
         </h1>
         <p className={styles.desc}>
           We create digital ideas that are bigger, bolder, braver and better. We
-          believe in good ideas flexibility and precission. We're world's Our
+          believe in good ideas flexibility and precission. We are
           Special Team best consulting & finance solution provider. Wide range
           of web and software development services.
         </p>
